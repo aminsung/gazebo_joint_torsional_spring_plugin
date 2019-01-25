7d40bb6bee799a818ea8926725a2cb84bfd29ee9
Min Sung Ahn <aminsung@gmail.com>
2019-01-24 22:31:01 -0800

    Cleaned up legacy code

--- src/gazebo_joint_torsional_spring.cpp
+++ src/gazebo_joint_torsional_spring.cpp
@@ -1,11 +1,6 @@
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
-// #include "gazebo/common/Assert.hh"
-// #include <gazebo/common/Event.hh>
 #include <gazebo/common/common.hh>
-// #include <gazebo/common/Plugin.hh>
-
-// #include <ros/ros.h>
 
 namespace gazebo
 {
@@ -13,48 +8,29 @@ namespace gazebo
 	{
 		public: TorsionalSpringPlugin() {}
 
-		
 		private:
-			// Pointer to model
 			physics::ModelPtr model;
-			// Pointer to SDF model
 			sdf::ElementPtr sdf;
-			// Pointer to joint
 			
 			physics::JointPtr joint;
+
 			// Set point
 			double setPoint;
+
 			// Spring constant
 			double kx;
+
 			// Pointer to update event connection
 			event::ConnectionPtr updateConnection;
-			
-			// Node handle
-			// std::unique_ptr<ros::NodeHandle> rosNode;
 
-
-
-		// Load is called by Gazebo when the plugin is inserted into simulation
 		public: void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
 		{
-			// if (!ros::isInitialized())
-			// {
-			// 	int argc = 0;
-   //              char **argv = NULL;
-   //              ros::init(argc, argv, "spring_joint", ros::init_options::NoSigintHandler);
-			// }
-		// 	GZ_ASSERT(_model != NULL, "Received NULL model pointer for JointTorsionalSping Plugin!")
-
-		// 	GZ_ASSERT(_sdf != NULL, "Received NULL SDF pointer for JointTorsionalSping Plugin!")
-
 			// Safety check
 			if (_model->GetJointCount() == 0)
 			{
-				std::cerr << "You have zero joints! Something is wrong! Not loading plugin.\n";
+				std::cerr << "You have zero joints! Something is wrong! Not loading plugin." << std::endl;
 				return;
 			}
-
-			// this->rosNode.reset(new ros::NodeHandle("spring_joint"));
 
 			// Store model pointer
 			this->model = _model;
@@ -65,7 +41,7 @@ namespace gazebo
 			if (_sdf->HasElement("joint"))
 				this->joint = _model->GetJoint(_sdf->Get<std::string>("joint"));
 			else
-				std::cout << "Must specify joint to apply a torsional spring at!\n";
+				std::cerr << "Must specify joint to apply a torsional spring at!\n";
 
 			this->kx = 0.0;
 			if (_sdf->HasElement("kx"))
@@ -74,16 +50,16 @@ namespace gazebo
 				printf("Torsional spring coefficient not specified! Defaulting to: %f\n", this->kx);
 
 			this->setPoint = 0.0;
+
 			if (_sdf->HasElement("set_point"))
 				this->setPoint = _sdf->Get<double>("set_point");
 			else
 				printf("Set point not specified! Defaulting to: %f\n", this->setPoint);
 
-			std::cout << "Loaded gazebo_joint_torsional_spring.\n";
+			std::cout << "Loaded gazebo_joint_torsional_spring." << std::endl;
 
 		}
 
-		
 		public: void Init()
 		{
 			// Listen to update event
@@ -99,6 +75,5 @@ namespace gazebo
 		
 	};
 
-	// Register the plugin such that Gazebo can call Load on this plugin
 	GZ_REGISTER_MODEL_PLUGIN(TorsionalSpringPlugin)
 }
